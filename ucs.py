3c02c08d5fc28c1b8c6c23375f7691ddca3ff676
asmaashimy <asmaashimy1@gmail.com>
2026-05-13 23:45:55 +0300

    Uninformed Search: UCS Algorithm

--- ucs.py
+++ ucs.py
@@ -1,19 +1,20 @@
 import heapq
 
+
 def run_ucs(MAZE, START, GOAL):
     ROWS = len(MAZE)
     COLS = len(MAZE[0])
-    
+
     pq = []
     heapq.heappush(pq, (0, START))
-    
+
     came_from = {START: None}
     cost_so_far = {START: 0}
     visited_order = []
 
     while pq:
         current_cost, current = heapq.heappop(pq)
-        
+
         if current not in visited_order:
             visited_order.append(current)
 
@@ -27,7 +28,7 @@ def run_ucs(MAZE, START, GOAL):
             if 0 <= nr < ROWS and 0 <= nc < COLS and MAZE[nr][nc] == 0:
                 neighbor = (nr, nc)
                 new_cost = cost_so_far[current] + 1
-                
+
                 if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
                     cost_so_far[neighbor] = new_cost
                     heapq.heappush(pq, (new_cost, neighbor))
@@ -40,5 +41,5 @@ def run_ucs(MAZE, START, GOAL):
             path.append(node)
             node = came_from[node]
         path.reverse()
-    
+
     return path, visited_order
